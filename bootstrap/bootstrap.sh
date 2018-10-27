89c6546bfc81d44fc061274c68fed52e1e00dded
Sam Rang <srang2010@gmail.com>
2018-10-27 13:04:29 -0400

    typo

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-
-# -- Not needed after initial startup --
+# -- not needed after initial startup --
 # minishift start --extra-clusterup-flags="--enable=*,service-catalog,template-service-broker,automation-service-broker"
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin srang
 oc new-project widget-factory
+oc delete is jenkins -n openshift
+oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
+oc tag openshift/jenkins:latest openshift/jenkins:2
+oc delete is jenkins -n openshift
+oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
+oc tag openshift/jenkins:latest openshift/jenkins:2
 oc process -f bootstrap.yaml | oc apply -f-

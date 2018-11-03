d300ed6725ca215d90f3d877fbb71bbab42bdea5
Sam Rang <srang2010@gmail.com>
2018-11-03 16:44:45 -0400

    refactor applier

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 # -- not needed after initial startup --
 # minishift start --extra-clusterup-flags="--enable=*,service-catalog,template-service-broker,automation-service-broker"
-oc login -u system:admin
-oc adm policy add-cluster-role-to-user cluster-admin srang
+# oc login -u system:admin
+# oc adm policy add-cluster-role-to-user cluster-admin srang
 oc new-project widget-factory
-oc delete is jenkins -n openshift
-oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
-oc tag openshift/jenkins:latest openshift/jenkins:2
-oc delete is jenkins -n openshift
-oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
-oc tag openshift/jenkins:latest openshift/jenkins:2
+# oc delete is jenkins -n openshift
+# oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
+# oc tag openshift/jenkins:latest openshift/jenkins:2
 oc process -f bootstrap.yaml | oc apply -f-

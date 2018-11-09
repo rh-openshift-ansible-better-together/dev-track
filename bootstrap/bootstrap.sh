e3db65a5823b471edbb2ebb551abf19883b34931
Sam Rang <srang2010@gmail.com>
2018-11-09 17:56:28 -0500

    test playbook

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
-# -- not needed after initial startup --
-# minishift start --extra-clusterup-flags="--enable=*,service-catalog,template-service-broker,automation-service-broker"
-# oc login -u system:admin
-# oc adm policy add-cluster-role-to-user cluster-admin srang
+
+## Section -1
+sudo yum install -y git apb
+git checkout git@github.com:srang/rh-openshift-ansible-broker-lab.git
+GIT_BASE="$(pwd)/rh-openshift-ansible-broker-lab"
+
+## Section 0
+cd $(GIT_BASE)/bootstrap
+
+## Section 1
+cd $(GIT_BASE)/database-provision-playbook
+ansible-playbook database-playbook.yml -i inventory/
+
+## Section 2
+oc login https://ec2-18-234-37-92.compute-1.amazonaws.com -u admin -p redhat01
 oc new-project widget-factory
-# oc delete is jenkins -n openshift
-# oc import-image jenkins --from=registry.access.redhat.com/openshift3/jenkins-2-rhel7 -n openshift --confirm
-# oc tag openshift/jenkins:latest openshift/jenkins:2
 oc process -f bootstrap.yml | oc apply -f-
+oc process template/mysql-ephemeral -p MYSQL_DATABASE=widgetfactory -n openshift | oc apply -f-
+oc start-build widget-jenkins-agent-pipeline
+
+## Section 3
 oc create is database-provision-apb -n openshift
 oc policy add-role-to-user system:image-builder system:serviceaccount:widget-factory:builder -n openshift
-#oc process template/mysql-ephemeral -p MYSQL_DATABASE=widgetfactory -n openshift | oc apply -f-
+oc start-build database-provision-apb
+## Pause
+apb broker bootstrap

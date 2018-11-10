0a96304a8500ee668fb3dca38903c35fd5169e9a
Sam Rang <srang2010@gmail.com>
2018-11-10 09:49:25 -0500

    section 1

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -16,7 +16,11 @@ cd ${GIT_BASE}/widget-factory
 sudo yum install -y rh-maven35 --enablerepo=rhel-server-rhscl-7-rpms
 scl enable rh-maven35 bash
 mvn clean install -Pdeploy
-SPRING_ACTIVE_PROFILES=canary java -jar target/widget-factory.jar
+SPRING_PROFILES_ACTIVE=canary java -jar target/widget-factory.jar &
+curl localhost:8080/widgets
+curl -H 'Content-type: application/json' -d '{ "label": "NEW01", "version": "V1", "description" "some new thing"}' localhost:8080/widgets
+curl localhost:8080/widgets
+mysql -u widget --password=widget01 widget
 
 
 ## Section 2

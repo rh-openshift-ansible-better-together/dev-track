08f2f062fd75ac951dd7b24d26cc6a503d760737
Austin Dewey <adewey@redhat.com>
2019-05-29 22:32:09 -0400

    adding install.sh script to install software on vms

---
+++ install.sh
@@ -0,0 +1,9 @@
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+curl -o /usr/local/bin/operator-sdk -L https://github.com/operator-framework/operator-sdk/releases/download/v0.5.0/operator-sdk-v0.5.0-x86_64-linux-gnu
+curl -o /tmp/oc.tar.gz -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar xvf /tmp/oc.tar.gz
+mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin/oc
+chmod 110 /usr/local/bin/operator-sdk
+chmod 110 /usr/local/bin/oc
